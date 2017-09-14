288d2f71bbc266311e33ed863a3c7d82cd80f04f
ywl48338 <ywl48338@ly.com>
2017-09-14 21:10:27 +0800

    更新工程

--- src/main/java/com/news/controller/TouTiaoController.java
+++ src/main/java/com/news/controller/TouTiaoController.java
@@ -1,5 +1,22 @@
 package com.news.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/toutiao")
 public class TouTiaoController {
+	private static Logger logger = Logger.getLogger(TouTiaoController.class);
+
+	@RequestMapping(value = "/test")
+	@ResponseBody
+	public String test() {
+		logger.info("测试日志");
+		return "ok";
+	}
+	
+	
 
 }
